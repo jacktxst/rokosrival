c00acbd5cb3001011601aec6f2c20fe3be5053ae
jacktxst <jacklathamlewis@gmail.com>
2026-09-04 19:34:24 -0500

    idk what i changed tbh

--- src/CardLibrary.js
+++ src/CardLibrary.js
@@ -2,4 +2,40 @@
 
 export class CardLibrary {
 	
+	constructor() {
+
+		this.cards = [
+
+			new CardBase({
+				name : "Card A"
+			}),
+
+			new CardBase({
+				name : "Card B"
+			}),
+
+			new CardBase({
+				name : "Card C"
+			}),
+
+			new CardBase({
+				name : "Card D"
+			}),
+
+			new CardBase({
+				name : "Card E"
+			}),
+
+			new CardBase({
+				name : "Card F"
+			}),
+
+			new CardBase({
+				name : "Card G"
+			})
+		]
+
+
+	}
+
 }
--- src/main.js
+++ src/main.js
@@ -2,37 +2,9 @@ import { CardPlayer } from './CardPlayer.js'
 import { CardGame } from './CardGame.js'
 import { CardDeck } from './CardDeck.js'
 import { CardBase } from './CardBase.js'
+import { CardLibrary } from './CardLibrary.js'
 
-let cards = [
-
-	new CardBase({
-		name : "Card A"
-	}),
-
-	new CardBase({
-		name : "Card B"
-	}),
-
-	new CardBase({
-		name : "Card C"
-	}),
-
-	new CardBase({
-		name : "Card D"
-	}),
-
-	new CardBase({
-		name : "Card E"
-	}),
-
-	new CardBase({
-		name : "Card F"
-	}),
-
-	new CardBase({
-		name : "Card G"
-	})
-]
+let cardLibrary = new CardLibrary()
 
 function p1Turn(){
 
@@ -48,8 +20,8 @@ function p2Turn(){
 
 }
 
-let p1 = new CardPlayer({turnCallback:p1Turn, deck:new CardDeck(cards)})
-let p2 = new CardPlayer({turnCallback:p2Turn, deck:new CardDeck(cards)})
+let p1 = new CardPlayer({turnCallback:p1Turn, deck:new CardDeck(cardLibrary.cards)})
+let p2 = new CardPlayer({turnCallback:p2Turn, deck:new CardDeck(cardLibrary.cards)})
 
 window.p1 = p1
 window.p2 = p2
